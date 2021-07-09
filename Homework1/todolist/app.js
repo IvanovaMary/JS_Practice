fbecf303d2c4ee3b1cdcdc90a101a6305d00a149
Ivanova Maria <ivanova20ist4@mail.ru>
2021-07-09 23:33:02 +0400

    Project1

---
+++ Homework1/todolist/app.js
@@ -0,0 +1,42 @@
+let buttonEnter = document.getElementById(`enter`);
+let userInput = document.getElementById(`userInput`);
+let ul = document.querySelector(`ul`)
+
+function inputLenght(){
+    return userInput.value.length > 0
+}
+
+function createTodo(){
+    let li = document.createElement(`li`)
+    li.appendChild(document.createTextNode(userInput.value))
+    ul.appendChild(li)
+    userInput.value = ``
+
+    let deleteButton = document.createElement(`button`)
+    deleteButton.appendChild(document.createTextNode(`X`))
+    deleteButton.addEventListener(`click`,deleteTodoitems)
+    li.appendChild(deleteButton)
+
+    li.onclick = () =>{
+        li.classList.toggle(`done`)
+    }
+
+    function deleteTodoitems(){
+        li.classList.add(`delete`)
+    }
+}
+
+function changeListAfterKeyPress(event){
+    if(inputLenght() && event.which === 13 ){
+        createTodo()
+    }
+    
+}
+
+buttonEnter.onclick = () =>{
+    if(inputLenght()){
+        createTodo()
+    }
+}
+
+userInput.addEventListener('keypress', changeListAfterKeyPress)
